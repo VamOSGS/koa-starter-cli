fe8143dd743dc4c8af6b3371b2a1d35e3fa91fc2
VamOSGS <gegham.samvelyan@hotmail.com>
2018-05-06 19:05:58 +0500

    Loader added

--- bin/cli.js
+++ bin/cli.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 
 const path = require('path');
-const { Command } = require('commander');
 const chalk = require('chalk');
+const ora = require('ora');
+const { Command } = require('commander');
+const { copySync, createPkg } = require('../lib');
 const pkg = require('../package.json');
-const { copySync } = require('../lib');
 
 const base = __dirname.replace(/bin/, '');
 const copy = path.join(base, 'copy');
-
+const tasks = ['Creating directory', ' Creating package.json'];
 let projectName;
 let projectDir;
 const program = new Command('koa-starter')
@@ -26,5 +27,13 @@ if (typeof projectName === 'undefined') {
   console.log(`  ${chalk.cyan(program.name())} ${chalk.green('<project-directory>')}`);
   process.exit();
 }
+const spinner = ora({ text: `[1/${tasks.length}] ${tasks[0]}` }).start();
 const copyDir = copySync(copy, projectDir);
 console.log(copyDir.status);
+if (!copyDir.done) process.exit();
+
+spinner.text = `[2/${tasks.length}] ${tasks[1]}`;
+createPkg(projectName, projectDir);
+setTimeout(() => {
+  process.exit();
+}, 500);
